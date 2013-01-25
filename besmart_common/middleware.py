69957b46ec41e1264bee7befed60393011ff27ed
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2013-01-25 15:30:02 +0600

    Added Terminal logging for SQL queries

--- besmart_common/middleware.py
+++ besmart_common/middleware.py
@@ -1,10 +1,29 @@
 # -*- encoding: utf-8 -*-
 from django.http import HttpResponseNotAllowed
 from django.core.urlresolvers import resolve, Resolver404
+from django.db import connection
+from django.conf import settings
+from sys import stdout
+
+
+# mapping of ansi escape sequences 
+KEYWORDS = { 
+    'time': '\033[1;31m[%s]\033[0m',
+    'text': '\033[2;37m%s\033[0m',
+    'SELECT': '\033[1;36m%s\033[0m',
+    'FROM': '\033[1;33m%s\033[0m',
+    'INNER': '\033[1;31;40m%s\033[0m',
+    'OUTER': '\033[1;34;40m%s\033[0m',
+    'JOIN': '\033[1;31;40m%s\033[0m',
+    'WHERE': '\033[1;32m%s\033[0m',
+    'INSERT': '\033[0;35m%s\033[0m',
+    'UPDATE': '\033[1;34m%s\033[0m',
+    'DISTINCT': '\033[0;32m%s\033[0m',
+}
 
 
 class AllowedMethodsMiddleware(object):
-    """ Checks request.method against accept_methods keyword in urls """
+    ''' Checks request.method against accept_methods keyword in urls '''
     def process_request(self, request):
         capture = False
 
@@ -22,3 +41,19 @@ class AllowedMethodsMiddleware(object):
 
             if False is capture:
                 return HttpResponseNotAllowed('Method not allowed')
+
+
+# the middleware class 
+class TerminalLoggingMiddleware:
+    def process_response(self, request, response):
+        if settings.DEBUG and stdout.isatty():
+            for query in connection.queries :
+                _sql = query['sql'] 
+                _sql = [ KEYWORDS['time'] % query['time'] ] + _sql.split( ) 
+                for index, item in enumerate( _sql ):
+                    if KEYWORDS.has_key( item ):
+                         _sql[index] = KEYWORDS[item] % item
+                    else:
+                         _sql[index] = KEYWORDS['text'] % item
+                print u' '.join( _sql) 
+        return response
